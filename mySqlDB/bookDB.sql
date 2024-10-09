6621b328f8d641d7fd8b1d9d2130cf47396a1973
Grant Cooper <69401337+Parzeval2@users.noreply.github.com>
2024-10-09 10:59:50 -0500

    more composing

--- mySqlDB/bookDB.sql
+++ mySqlDB/bookDB.sql
@@ -1,23 +1,6 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: localhost    Database: bookstore
--- ------------------------------------------------------
--- Server version	5.6.35-log
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `book_category_relationship`
---
+CREATE DATABASE IF NOT EXISTS bookstore CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE bookstore
 
 DROP TABLE IF EXISTS `book_category_relationship`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
